797377993670e226497688871f38d2c2e2007e15
cfarre <cfarre@microsoft.com>
2019-01-10 15:52:59 -0800

    fixed bash script

--- performance/deployment/grafana/GrafanaResource/GrafanaResource/AzureDataSource.sh
+++ performance/deployment/grafana/GrafanaResource/GrafanaResource/AzureDataSource.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-
- sudo /opt/bitnami/ctlscript.sh restart grafana 
- sudo su grafana -c '/opt/bitnami/grafana/bin/grafana-cli --pluginsDir /opt/bitnami/grafana/data/plugins/ plugins install grafana-azure-monitor-datasource'
+sudo su grafana -c '/opt/bitnami/grafana/bin/grafana-cli --pluginsDir /opt/bitnami/grafana/data/plugins/ plugins install grafana-azure-monitor-datasource'
+sudo /opt/bitnami/ctlscript.sh restart grafana 
