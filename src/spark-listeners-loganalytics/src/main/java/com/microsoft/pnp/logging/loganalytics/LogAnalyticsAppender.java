16e95bcb9456b18938651909176545755d8fd666
Andrew Oakley <anoakley@microsoft.com>
2019-04-18 22:09:36 +0000

    Add support for application id, executor id, and Databricks cluster id in log4j appender

--- src/spark-listeners-loganalytics/src/main/java/com/microsoft/pnp/logging/loganalytics/LogAnalyticsAppender.java
+++ src/spark-listeners-loganalytics/src/main/java/com/microsoft/pnp/logging/loganalytics/LogAnalyticsAppender.java
@@ -6,8 +6,14 @@ import com.microsoft.pnp.client.loganalytics.LogAnalyticsSendBufferClient;
 import com.microsoft.pnp.logging.JSONLayout;
 import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.Layout;
+import org.apache.log4j.helpers.LogLog;
 import org.apache.log4j.spi.Filter;
 import org.apache.log4j.spi.LoggingEvent;
+import org.apache.spark.SparkContext;
+import org.apache.spark.SparkEnv;
+import org.apache.spark.sql.RuntimeConfig;
+import org.apache.spark.sql.SparkSession;
+import scala.runtime.AbstractFunction0;
 
 import static com.microsoft.pnp.logging.JSONLayout.TIMESTAMP_FIELD_NAME;
 
@@ -30,6 +36,18 @@ public class LogAnalyticsAppender extends AppenderSkeleton {
     private String logType = DEFAULT_LOG_TYPE;
     private LogAnalyticsSendBufferClient client;
 
+    private String applicationId;
+    private String clusterId;
+    private String executorId;
+
+    private AbstractFunction0<SparkSession> orElse = new AbstractFunction0<SparkSession>() {
+
+        @Override
+        public SparkSession apply() {
+            return null;
+        }
+    };
+
     public LogAnalyticsAppender() {
         this.addFilter(ORG_APACHE_HTTP_FILTER);
     }
@@ -40,6 +58,35 @@ public class LogAnalyticsAppender extends AppenderSkeleton {
                 new LogAnalyticsClient(this.workspaceId, this.secret),
                 this.logType
         );
+
+        this.executorId = System.getProperty(
+                "local.spark.executor.id",
+                null);
+        this.clusterId = System.getProperty(
+                "local.spark.databricks.clusterUsageTags.clusterId",
+                null
+        );
+
+        // If we are on the executor, we can get the application id
+        if (this.executorId != "driver") {
+            this.applicationId = System.getProperty(
+                    "local.spark.application.id",
+                    null
+            );
+        }
+//        // We need to kick Spark to get some of the stuff
+//        try {
+//            SparkSession sparkSession = SparkSession.builder().getOrCreate();
+//            SparkContext sparkContext = sparkSession.sparkContext();
+//            RuntimeConfig runtimeConfig = sparkSession.conf();
+//            SparkEnv env = SparkEnv.get();
+//            this.applicationId = sparkContext.applicationId();
+//            this.executorId = env.executorId();
+//            this.clusterId = runtimeConfig
+//                    .get("spark.databricks.clusterUsageTags.clusterId", null);
+//        } catch (Exception ex) {
+//            LogLog.error("Error with SparkSession", ex);
+//        }
     }
 
     @Override
@@ -48,6 +95,43 @@ public class LogAnalyticsAppender extends AppenderSkeleton {
             this.setLayout(new JSONLayout());
         }
 
+        // TODO - If we are on the driver, we may or may not have an application id yet
+        String localApplicationId = null;
+
+        if ("driver".equals(this.executorId)) {
+            // We need to check each time we log since on the driver it's a different lifecycle
+            // See if we have a session
+            SparkSession sparkSession = SparkSession.getActiveSession().getOrElse(
+                    new AbstractFunction0<SparkSession>() {
+                        @Override
+                        public SparkSession apply() {
+                            return SparkSession.getDefaultSession().getOrElse(new AbstractFunction0<SparkSession>() {
+                                @Override
+                                public SparkSession apply() {
+                                    return null;
+                                }
+                            });
+                        }
+                    });
+            if (sparkSession != null) {
+                localApplicationId = sparkSession.sparkContext().applicationId();
+            }
+        } else {
+            // If we are not on the driver, the applicationId should have been given to us in
+            // system properties, so this should be okay
+            localApplicationId = this.applicationId;
+        }
+
+        // Add extra stuff
+        if (localApplicationId != null) {
+            loggingEvent.setProperty("applicationId", localApplicationId);
+        }
+        if (this.executorId != null) {
+            loggingEvent.setProperty("executorId", this.executorId);
+        }
+        if (this.clusterId != null) {
+            loggingEvent.setProperty("clusterId", this.clusterId);
+        }
         String json = this.getLayout().format(loggingEvent);
         this.client.sendMessage(json, TIMESTAMP_FIELD_NAME);
     }
--- src/spark-listeners/scripts/listeners.sh
+++ src/spark-listeners/scripts/listeners.sh
@@ -10,3 +10,26 @@ echo "Merging metrics.properties"
 cat "$STAGE_DIR/metrics.properties" <(echo) /databricks/spark/conf/metrics.properties > /databricks/spark/conf/tmp.metrics.properties || { echo "Error merging metrics.properties"; exit 1; }
 mv /databricks/spark/conf/tmp.metrics.properties /databricks/spark/conf/metrics.properties || { echo "Error writing metrics.properties"; exit 1; }
 echo "Merged metrics.properties successfully"
+
+echo "BEGIN: Updating Executor log4j properties file with Log analytics appender"
+sed -i 's/log4j.rootCategory=.*/&, logAnalyticsAppender/g' /databricks/spark/dbconf/log4j/executor/log4j.properties
+tee -a /databricks/spark/dbconf/log4j/executor/log4j.properties << EOF
+# logAnalytics
+log4j.appender.logAnalyticsAppender=com.microsoft.pnp.logging.loganalytics.LogAnalyticsAppender
+log4j.appender.logAnalyticsAppender.layout=com.microsoft.pnp.logging.JSONLayout
+log4j.appender.logAnalyticsAppender.layout.LocationInfo=false
+EOF
+echo "END: Updating Executor log4j properties file with Log analytics appender"
+
+echo "BEGIN: Updating Driver log4j properties file with Log analytics appender"
+sed -i 's/log4j.rootCategory=.*/&, logAnalyticsAppender/g' /databricks/spark/dbconf/log4j/driver/log4j.properties
+tee -a /databricks/spark/dbconf/log4j/driver/log4j.properties << EOF
+# logAnalytics
+log4j.appender.logAnalyticsAppender=com.microsoft.pnp.logging.loganalytics.LogAnalyticsAppender
+log4j.appender.logAnalyticsAppender.layout=com.microsoft.pnp.logging.JSONLayout
+log4j.appender.logAnalyticsAppender.layout.LocationInfo=false
+EOF
+echo "END: Updating Driver log4j properties file with Log analytics appender"
+
+# Promote the DB_CLUSTER_ID into the environment so we can access it later.
+echo CLUSTER_ID=$DB_CLUSTER_ID >> /etc/environment
