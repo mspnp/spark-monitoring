95518a6fab1ff765cee163fdaf3c9314a36faa34
Enrique Catala <enrique@enriquecatala.com>
2021-11-19 10:16:36 +0100

    parameter validation

--- upload_with_dbfs.sh
+++ upload_with_dbfs.sh
@@ -1,4 +1,8 @@
+if [[ $# -eq 0 ]] ; then
+    echo 'Parameter0 not supplied. Please include the version of the library. For example: ./upload_with_dbfs.sh 3.1.2_2.12-1.0.0. '
+    exit 1
+fi
 dbfs mkdirs dbfs:/databricks/spark-monitoring
-dbfs cp --overwrite src/target/spark-listeners_3.1.2_2.12-1.0.0.jar dbfs:/databricks/spark-monitoring/
-dbfs cp --overwrite src/target/spark-listeners-loganalytics_3.1.2_2.12-1.0.0.jar dbfs:/databricks/spark-monitoring/
+dbfs cp --overwrite src/target/spark-listeners_$1.jar dbfs:/databricks/spark-monitoring/
+dbfs cp --overwrite src/target/spark-listeners-loganalytics_$1.jar dbfs:/databricks/spark-monitoring/
 dbfs cp --overwrite src/spark-listeners/scripts/spark-monitoring.sh dbfs:/databricks/spark-monitoring/
