a0d53eb4a6caaa786910156aa00ccad4f05096c9
Rick Hallihan <1796255+hallihan@users.noreply.github.com>
2022-07-01 14:14:11 -0400

    Version Updates
    - Remove support for Spark 2.x, 3.2.0

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 echo 'hosts: files dns' > /etc/nsswitch.conf
 echo "127.0.0.1   $(hostname)" >> /etc/hosts
 
-MAVEN_PROFILES=( "scala-2.11_spark-2.4.5" "scala-2.12_spark-3.0.1" "scala-2.12_spark-3.1.2" "scala-2.12_spark-3.2.0" "scala-2.12_spark-3.2.1")
+MAVEN_PROFILES=( "scala-2.12_spark-3.0.1" "scala-2.12_spark-3.1.2" "scala-2.12_spark-3.2.1" )
 for MAVEN_PROFILE in "${MAVEN_PROFILES[@]}"
 do
     mvn -f /spark-monitoring/src/pom.xml install -P ${MAVEN_PROFILE} "$@"
