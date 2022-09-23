d0fd31a10d1a99cedd25e8abfaf7d3a92453356e
Jeff Harrison <jharrison@nexthealthtechnologies.com>
2022-09-23 14:29:36 -0600

    add quotes

--- src/spark-listeners/scripts/spark-monitoring.sh
+++ src/spark-listeners/scripts/spark-monitoring.sh
@@ -45,7 +45,7 @@ export AZ_RSRC_NAME=
 # or where the Message matches the message regex.  If both are specified, then both must be matched for the log to be sent.
 # Commented examples below will only log messages where the logger name is com.microsoft.pnp.samplejob.StreamingQueryListenerSampleJob or begins with
 # org.apache.spark.util.Utils, or where the Message ends with the string 'StreamingQueryListenerSampleJob' or begins with the string 'FS_CONF_COMPAT'.
-export LA_SPARKLOGGINGEVENT_NAME_REGEX=${SPARK_MONITORING_EVENT_NAME_REGEX}
+export LA_SPARKLOGGINGEVENT_NAME_REGEX="${SPARK_MONITORING_EVENT_NAME_REGEX}"
 # export LA_SPARKLOGGINGEVENT_MESSAGE_REGEX=".*StreamingQueryListenerSampleJob|FS_CONF_COMPAT.*"
 
 
